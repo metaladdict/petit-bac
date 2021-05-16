9ab88ec523d9ec07e96511e879e1fe33c27dd54d
metaladdict <metaladdict@users.noreply.github.com>
2021-05-16 02:58:26 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(lettres.charAt(randint(0, lettres.length - 1)))
+})
+let lettres = ""
+lettres = "ABCDEFGHIJKLMNOPRSTUVZ"
+basic.forever(function () {
+	
+})
