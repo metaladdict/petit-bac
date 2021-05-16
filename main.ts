03ea6bdbc08720e8bc916b87db0c269bcd20627b
metaladdict <metaladdict@users.noreply.github.com>
2021-05-16 02:58:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString(lettres.charAt(randint(0, lettres.length - 1)))
 })
 let lettres = ""
-lettres = "ABCDEFGHIJKLMNOPRSTUVZ"
+lettres = "ABCDEFGHIJKLMNOPRSTUVYZ"
 basic.forever(function () {
 	
 })
