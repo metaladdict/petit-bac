a43cdfe3e1e23f6387bc714ba9433c85135797ca
metaladdict <metaladdict@users.noreply.github.com>
2021-05-17 21:05:44 +0200

    retirer Y

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showIcon(IconNames.Target)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showString(lettres.charAt(randint(0, lettres.length - 1)))
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showIcon(IconNames.Ghost)
+    basic.pause(100)
+    basic.clearScreen()
+})
 let lettres = ""
-lettres = "ABCDEFGHIJKLMNOPRSTUVYZ"
+input.setSoundThreshold(SoundThreshold.Loud, 155)
+lettres = "ABCDEFGHIJKLMNOPRSTUVZ"
 basic.forever(function () {
 	
 })
